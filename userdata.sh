b94a4509ecec1da92190417b5f7e79f78ae2ba10
Eyüp ilhan <117114566+eyupilhan@users.noreply.github.com>
2026-06-25 17:48:27 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,16 +1,23 @@
+```bash
 #!/bin/bash
+
 apt-get update -y
-apt-get install git -y
-apt-get install python3 -y
+
+apt-get install -y git python3 python3-pip python3-dev libmysqlclient-dev
+
 cd /home/ubuntu/
-TOKEN='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-git clone https://$TOKEN@github.com/T01-2133/Project10-AWS-Capstone.git
-cd /home/ubuntu/Project10-AWS-Capstone
-apt install python3-pip -y
-apt-get install python3.7-dev libmysqlclient-dev -y
+
+git clone https://github.com/eyupilhan/aws-devops-capstone-project.git
+
+cd /home/ubuntu/aws-devops-capstone-project
+
 pip3 install -r requirements.txt
-cd /home/ubuntu/Project10-AWS-Capstone/src
+
+cd /home/ubuntu/aws-devops-capstone-project/src
+
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
+
 python3 manage.py runserver 0.0.0.0:80
+```
